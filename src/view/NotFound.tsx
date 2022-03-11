00961f46304eb6a31bed74b895a9a003119d893f
xylonx <x956200361@gmail.com>
2022-03-11 09:41:37 +0800

    :construction: (notFound) 404 page

---
+++ src/view/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+export function NotFoundPage() {
+    return (
+        <Box>
+            <Typography>nothing in this page</Typography>
+        </Box>
+    );
+}
